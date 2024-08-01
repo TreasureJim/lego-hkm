049cb382321864cde0a4c13c618aa76f58239798
Liam Boreback <boreback.work@gmail.com>
2024-08-01 10:03:44 +0200

    Finish path finding algorithm

--- src/custom_pathfinding.cpp
+++ src/custom_pathfinding.cpp
@@ -11,11 +11,13 @@ class PathFinding {
 	Eigen::Vector3d limits_min;
 	Eigen::Vector3d limits_max;
 
+	inline bool pos_within_bounding_box(Eigen::Vector3d pos);
+	Eigen::Vector3d ray_box_intersection(Eigen::Vector3d pos, Eigen::Vector3d dir);
 	std::optional<Eigen::Vector3d> find_last_valid_pos(const Eigen::Vector3d& start_pos, const Eigen::Vector3d& end_pos);
 
   public:
 	PathFinding(Eigen::Vector3d limits_min = DEFAULT_LIM_MIN, Eigen::Vector3d limits_max = DEFAULT_LIM_MAX);
-	std::vector<Eigen::Vector3d> find_path(Eigen::Vector3d start_pos, Eigen::Vector3d goal_pos);
+	std::optional<std::vector<Eigen::Vector3d>> find_path(Eigen::Vector3d start_pos, Eigen::Vector3d goal_pos);
 };
 
 PathFinding::PathFinding(Eigen::Vector3d limits_min, Eigen::Vector3d limits_max) {
@@ -23,17 +25,13 @@ PathFinding::PathFinding(Eigen::Vector3d limits_min, Eigen::Vector3d limits_max)
 	this->limits_max = limits_max;
 }
 
-inline bool pos_within_bounding_box(Eigen::Vector3d limits_min, Eigen::Vector3d limits_max, Eigen::Vector3d pos) {
-	return pos.x() < limits_min.x() || pos.x() > limits_max.x() ||
-		pos.y() < limits_min.y() || pos.y() > limits_max.y() ||
-		pos.z() < limits_min.z() || pos.z() > limits_max.z();
+inline bool PathFinding::pos_within_bounding_box(Eigen::Vector3d pos) {
+	return pos.x() < this->limits_min.x() || pos.x() > this->limits_max.x() ||
+		pos.y() < this->limits_min.y() || pos.y() > this->limits_max.y() ||
+		pos.z() < this->limits_min.z() || pos.z() > this->limits_max.z();
 }
 
 std::optional<Eigen::Vector3d> PathFinding::find_last_valid_pos(const Eigen::Vector3d& start_pos, const Eigen::Vector3d& end_pos) {
-	// check if within limits
-	if (!pos_within_bounding_box(this->limits_min, this->limits_max, start_pos)) return std::nullopt;
-	if (!pos_within_bounding_box(this->limits_min, this->limits_max, end_pos)) return std::nullopt;
-
 	// find first invalid position
 	Eigen::Vector3d first_invalid;
 	bool no_invalid = true;
@@ -57,9 +55,34 @@ std::optional<Eigen::Vector3d> PathFinding::find_last_valid_pos(const Eigen::Vec
 	return std::nullopt;
 }
 
-// TODO: implement function for determining intersection position on bounding box from origin and direction
+/// Computes the intersection for a bounding box and a ray given that the ray starts inside the box 
+Eigen::Vector3d PathFinding::ray_box_intersection(Eigen::Vector3d pos, Eigen::Vector3d dir) {
+    double t_min_x = (this->limits_min.x() - pos.x()) / dir.x();
+    double t_max_x = (this->limits_max.x() - pos.x()) / dir.x();
 
-std::vector<Eigen::Vector3d> PathFinding::find_path(Eigen::Vector3d start_pos, Eigen::Vector3d goal_pos) {
+    double t_min_y = (this->limits_min.y() - pos.y()) / dir.y();
+    double t_max_y = (this->limits_max.y() - pos.y()) / dir.y();
+
+    double t_min_z = (this->limits_min.z() - pos.z()) / dir.z();
+    double t_max_z = (this->limits_max.z() - pos.z()) / dir.z();
+
+    // Ensure t_min is the entry point and t_max is the exit point for each axis
+    if (t_min_x > t_max_x) std::swap(t_min_x, t_max_x);
+    if (t_min_y > t_max_y) std::swap(t_min_y, t_max_y);
+    if (t_min_z > t_max_z) std::swap(t_min_z, t_max_z);
+
+    // Find the largest t_min, which is the first intersection with the cube's surface
+    double t_hit = std::max({t_min_x, t_min_y, t_min_z});
+
+    // Compute the intersection point
+    return pos + t_hit * dir;
+}
+
+std::optional<std::vector<Eigen::Vector3d>> PathFinding::find_path(Eigen::Vector3d start_pos, Eigen::Vector3d goal_pos) {
+	// check if within limits
+	if (!pos_within_bounding_box(start_pos)) return std::nullopt;
+	if (!pos_within_bounding_box(goal_pos)) return std::nullopt;
+
 	std::vector<Eigen::Vector3d> path = {start_pos};
 	
 	while(true) {
@@ -69,7 +92,8 @@ std::vector<Eigen::Vector3d> PathFinding::find_path(Eigen::Vector3d start_pos, E
 		path.push_back(pos_opt.value());
 
 		auto vec_cross = (path.end()[-2] - path.back()).cross(UP_VEC).normalized();
-		auto test_pos // TOOD: finish
+		auto intersection_point = this->ray_box_intersection(path.back(), vec_cross);
+		path.push_back(path.back() + vec_cross * (intersection_point - path.back()).norm() / 2);
 	}
 
 	path.push_back(goal_pos);
