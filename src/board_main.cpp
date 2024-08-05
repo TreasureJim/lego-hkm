23fda9ad95f62243119679888935a480b89403a0
Liam Boreback <boreback.work@gmail.com>
2024-08-05 17:04:43 +0200

    Fix board randomization (based on time)

--- src/board_main.cpp
+++ src/board_main.cpp
@@ -1,9 +1,14 @@
+#include <boost/date_time/posix_time/posix_time_duration.hpp>
+#include <boost/thread/pthread/thread_data.hpp>
+#include <chrono>
+#include <ctime>
 #include <array>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <random>
+#include <stdio.h>
 
 #define RAD_TO_DEG 57.295779513
 
@@ -11,13 +16,22 @@
 #include "mark2_0_fixed.hpp"
 #include "robot.hpp"
 
+std::chrono::system_clock::rep time_since_epoch(){
+    static_assert(
+        std::is_integral<std::chrono::system_clock::rep>::value,
+        "Representation of ticks isn't an integral value."
+    );
+    auto now = std::chrono::system_clock::now().time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::seconds>(now).count();
+}
+
+std::default_random_engine re(time_since_epoch());
 Eigen::Vector3d random_valid_cart_pos(void) {
   double joint_angles[4];
   for (int i = 0; i < 4; i++) {
     std::uniform_real_distribution<double> unif(mark2_0_fixed.joint_lims[i][0] +
                                                     0.0001,
                                                 mark2_0_fixed.joint_lims[i][1]);
-    std::default_random_engine re;
     joint_angles[i] = unif(re);
   }
 
@@ -30,8 +44,12 @@ Eigen::Vector3d random_valid_cart_pos(void) {
 
 int main(int argc, char *argv[]) {
   Robot robot;
-  if (robot.error)
+  if (robot.error) {
+    fprintf(stderr, "[ERROR] Could not initialise robot.\n");
     return 0;
+  }
+
+  boost::this_thread::sleep(boost::posix_time::seconds(1));
 
   robot.move_linear( random_valid_cart_pos());
 }
