6808fbed8827b19f084b7f11cf5c32e577abf41c
Liam Boreback <boreback.work@gmail.com>
2024-08-12 16:48:00 +0200

    Implement juliet server library to issue commands

--- src/chan-gen-types/defs/motion_types.jl
+++ src/chan-gen-types/defs/motion_types.jl
@@ -1,5 +1,13 @@
 using StaticArrays
 
+"""
+A struct representing the status of a command
+
+`status`:
+- error = -1
+- in progress = 0
+- complete = 1
+"""
 struct MotionId
     id::SVector{16, UInt8}
     status::Int8
@@ -47,90 +55,5 @@ end
 struct MovePos
     motion_id::SVector{16, UInt8}
     target::RobTarget
-end
-
-struct Velocity
-    motion_id::SVector{16, UInt8}
-    percentage::Float64
-    path_speed::Float64
-    orientation_speed::Float64
-    external_axis_l::Float64
-    external_axis_j::Float64
-end
-
-#########
-
-struct BlendValue
-    percentage::Float64
-    distance::Float64
-    vel_const::Float64
-end
-
-struct Blend
-    motion_id::SVector{16, UInt8}
-    blend_type::UInt16
-    blend_value::BlendValue
-end
-
-struct JointTarget
-    j1::Float64
-    j2::Float64
-    j3::Float64
-    j4::Float64
-    j5::Float64
-    j6::Float64
-end
-
-struct JointPos
-    j1::Float64
-    j2::Float64
-    j3::Float64
-    j4::Float64
-    j5::Float64
-    j6::Float64
-    j7::Float64
-    j8::Float64
-    j9::Float64
-    j10::Float64
-    j11::Float64
-    j12::Float64
-    j13::Float64
-    j14::Float64
-    j15::Float64
-end
-
-struct Trigger
-    trigger_id::SVector{16, UInt8}
-end
-
-struct RobotIOMessage
-    handle::Int64
-    val::Int8
-end
-
-struct OnParameter
-    percentage::Float64
-    time_ms::Float64
-    trigger_id::SVector{16, UInt8}
-end
-
-struct OnDistance
-    start_type::UInt16
-    distance::Float64
-    time_ms::Float64
-    trigger_id::SVector{16, UInt8}
-end
-
-struct MotionInfo
-    motion_id::SVector{16, UInt8}
-    nr_expected_params::Int64
-end
-
-struct KernelError
-    error_id::UInt64
-    error_level::UInt64
-    error_info_ch::String
-    error_info_en::String
-    error_emergency::UInt64
 end
 
