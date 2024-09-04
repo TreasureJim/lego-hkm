9183731011b44a30330d83e2ef1eb3f5eb6c3a15
Liam Boreback <boreback.work@gmail.com>
2024-09-04 19:01:15 +0200

    Continue IMotion

--- src/trajectory_planner.cpp
+++ src/trajectory_planner.cpp
@@ -1,56 +1,93 @@
+#include "pathfinding.hpp"
 #include "juliet_comms.hpp"
 #include "math/3d_circle.hpp"
 #include <Eigen/Dense>
+#include <vector>
 
 Eigen::Vector3d robtarget_to_vector(robtarget &r) { return Eigen::Vector3d(r.x, r.y, r.z); }
 
 class IMotion {
-  Eigen::Vector3d origin_pos;
-  motion_u motion;
+protected:
+	const Eigen::Vector3d origin_pos;
 
 public:
-  IMotion(Eigen::Vector3d origin_pos);
-  virtual Eigen::Vector3d GetPoint(float t);
+	IMotion(Eigen::Vector3d origin_pos);
+	virtual Eigen::Vector3d GetPoint(float t);
 };
 
-IMotion::IMotion(Eigen::Vector3d origin_pos) {
-  this->origin_pos = origin_pos;
+IMotion::IMotion(Eigen::Vector3d origin_pos): origin_pos(origin_pos) {}
+
+class MotionCircle : public IMotion {
+	Circle_3D circle_3d;
+
+  public:
+	MotionCircle(Eigen::Vector3d origin_pos, movecircular circle);
+	Eigen::Vector3d GetPoint(float t) override;
+};
+
+MotionCircle::MotionCircle(Eigen::Vector3d origin_pos, movecircular circle)
+	: IMotion(origin_pos), circle_3d(origin_pos, robtarget_to_vector(circle.apos), robtarget_to_vector(circle.target)) {
 }
 
-class MotionCircle: public IMotion {
-Circle_3D circle_3d;
+Eigen::Vector3d MotionCircle::GetPoint(float t) { return this->circle_3d.get_circle_coord(t); }
+
+class MotionArc : public IMotion {
+	Circle_3D circle_3d;
+
+  public:
+	MotionArc(Eigen::Vector3d origin_pos, movecircular circle);
+  Eigen::Vector3d GetPoint(float t) override;
+};
+
+MotionArc::MotionArc(Eigen::Vector3d origin_pos, movecircular circle)
+	: IMotion(origin_pos), circle_3d(origin_pos, robtarget_to_vector(circle.apos), robtarget_to_vector(circle.target)) {
+}
+
+Eigen::Vector3d MotionArc::GetPoint(float t) { return this->circle_3d.get_arc_coord(t); }
+
+class MotionLinear : public IMotion {
+  Eigen::Vector3d target;
 
 public:
-  MotionCircle(movecircular circle);
+  MotionLinear(Eigen::Vector3d origin_pos, Eigen::Vector3d target);
+  Eigen::Vector3d GetPoint(float t) override;
 };
 
-MotionCircle::MotionCircle(Eigen::Vector3d origin_pos, movecircular circle): IMotion(origin_pos) {
-  this->circle_3d = Circle_3D(origin_pos, circle.apos, circle.target);
+MotionLinear::MotionLinear(Eigen::Vector3d origin_pos, Eigen::Vector3d target): IMotion(origin_pos), target(target) {}
+
+Eigen::Vector3d MotionLinear::GetPoint(float t) {
+  return this->origin_pos + (this->target - this->origin_pos) * t;
 }
 
-Eigen::Vector3d MotionCircle::GetPoint(float t) {
-  return this->circle_3d.get_circle_coord(t);
-}
-
-class MotionArc: public IMotion {
-Circle_3D circle_3d;
+class MotionPath : public IMotion {
+  std::vector<Eigen::Vector3d> path;
+  std::vector<float> path_percentages;
 
 public:
-  MotionCircle(movearc circle);
+  MotionPath(Eigen::Vector3d origin_pos, Eigen::Vector3d target);
+  Eigen::Vector3d GetPoint(float t) override;
 };
 
-MotionCircle::MotionCircle(Eigen::Vector3d origin_pos, movecircular circle): IMotion(origin_pos) {
-  this->circle_3d = Circle_3D(origin_pos, circle.apos, circle.target);
+MotionPath::MotionPath(Eigen::Vector3d origin_pos, Eigen::Vector3d target): IMotion(origin_pos), path(PathFinding().find_path(origin_pos, target).value()) {
+  if (this->path.empty()) throw "No path found.";
+
+  float total_size = 0.0; 
+  for (int i = 1; i < this->path.size(); i++) {
+    this->total_size += (this->path[i] - this->path[i - 1]).norm();
+    this->path_percentages.push_back(this->total_size);
+  }
 }
 
-Eigen::Vector3d MotionCircle::GetPoint(float t) {
-  return this->circle_3d.get_arc_coord(t);
+Eigen::Vector3d MotionPath::GetPoint(float t) {
+  float distance = this->total_size * t;
+  
+  // Find how far along in the path
+  int path_i;
+  for (path_i = 0; path_i < this->path_percentages.size(); path_i++) {
+    if (this->path_percentages[path_i] > distance) continue;
+    
+    break;
+  }
+
+  return (this->path[i + 1] - this->path[i]) * 
 }
-
-class MotionLinear: public IMotion {
-
-};
-
-class MotionPath: public IMotion {
-
-};
