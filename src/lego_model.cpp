7ca36da978a06255d6287b1d3a1eb277000c6ca8
Liam Boreback <boreback.work@gmail.com>
2024-10-09 13:00:58 +0200

    Update lego model with new measurements

--- src/lego_model.cpp
+++ src/lego_model.cpp
@@ -3,18 +3,18 @@
 
 struct agile_pkm_model lego_model = {
 	.bh = NULL,
-	.L0 = 0.64275, // height from floor to bottom part of elbow // UNFINISHED
-	.L1 = 0.945, // length from servo 1 to end of elbow // UNFINISHED
+	.L0 = 0.10234, // height from floor to bottom part of elbow 
+	.L1 = 0.18, // length from servo 1 to end of elbow 
 	.L2 = 0.136 + 0.015, // lenght of elbow + D2
 	.L3 = 0.03, //
 	.L3b = 0.033, //
 	.Lpar = 0.143, //
 	.a2 = 0.01, // tool length
 	.d2 = 0.015, //
-	.ax2x = 0.1, // UNFINISHED
-	.ax2y = 0.1, // UNFINISHED
+	.ax2x = 0.018, 
+	.ax2y = 0.020,
 	.ax2z = 0.0315 - 0.0335, // 
 	.ax3y = -0.065, // length from servo 1 to elbow rotation axis on x axis
 	.q2_offs = -0.07398341092526937,
-	.joint_lims = {{-3.6651914291880923, 2.6179938779914944}, {-1.4178869349609031, 0.885947677671612}, {-1.2217304763960306, 1.2217304763960306}, {-3.141592653589793, 3.141592653589793}},
+	.joint_lims = {{-1.56, -1.56}, {-0.52, 0.52}, {-0.78, 0.78}, {-3.141592653589793, 3.141592653589793}},
 };
