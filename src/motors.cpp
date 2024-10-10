ffd7ca00dcffa3f49206e790d4a5c3d9bb1966fe
Liam Boreback <boreback.work@gmail.com>
2024-10-10 14:05:29 +0200

    Fix startup to use calibration values

--- src/motors.cpp
+++ src/motors.cpp
@@ -16,8 +16,7 @@ extern "C" {
 
 std::array<std::array<double, 2>, 3> motor_offset_values;
 
-std::array<double, 4> DEFAULT_JOINT_ANGLES = {lego_model.joint_lims[0][0] + 0.01, lego_model.joint_lims[1][0] + 0.01, lego_model.joint_lims[2][0] + 0.01, lego_model.joint_lims[3][0] + 0.01};
-std::array<double, 4> current_joint_angles = DEFAULT_JOINT_ANGLES;
+std::array<double, 4> current_joint_angles;
 
 void matrix_to_pos(double matrix[4][4], double pos[3]) {
 	pos[0] = matrix[0][3];
@@ -63,17 +62,30 @@ double joint_angle_to_libservo_value(double joint_angle, uint8_t motor) {
 	return norm_angle * (motor_lim[1] - motor_lim[0]) + motor_lim[0];
 }
 
+double motor_angle_to_joint_angle(int motor, double motor_angle) {
+	const std::array<double, 2> motor_lim = motor_offset_values[motor];
+	double norm_value = (motor_angle - motor_lim[0]) / (motor_lim[1] - motor_lim[0]);
+
+	const double* lego_lim = lego_model.joint_lims[motor];
+	return norm_value * (lego_lim[1] - lego_lim[0]) + lego_lim[0];
+}
+
 void motor_reset_angle() {
-	current_joint_angles[0] = DEFAULT_JOINT_ANGLES[0];
-	current_joint_angles[1] = DEFAULT_JOINT_ANGLES[1];
-	current_joint_angles[2] = DEFAULT_JOINT_ANGLES[2];
-	current_joint_angles[3] = DEFAULT_JOINT_ANGLES[3];
+	double motor_angles[4];
+	for (int i = 0; i < 4; i++) {
+		motor_angles[i] = motor_offset_values[i][0] + (motor_offset_values[i][1] - motor_offset_values[i][0]) / 2.0;
+	}
+
+	current_joint_angles[0] = motor_angle_to_joint_angle(0, motor_angles[0]);
+	current_joint_angles[1] = motor_angle_to_joint_angle(1, motor_angles[1]);
+	current_joint_angles[2] = motor_angle_to_joint_angle(2, motor_angles[2]);
+	current_joint_angles[3] = motor_angle_to_joint_angle(3, motor_angles[3]);
 
 	for (int i = 0; i < 50; i++) {
-		rc_servo_send_pulse_normalized(1, joint_angle_to_libservo_value(current_joint_angles[0], 0));
-		rc_servo_send_pulse_normalized(2, joint_angle_to_libservo_value(current_joint_angles[1], 1));
-		rc_servo_send_pulse_normalized(3, joint_angle_to_libservo_value(current_joint_angles[2], 2));
-		rc_servo_send_pulse_normalized(4, joint_angle_to_libservo_value(current_joint_angles[3], 3));
+		rc_servo_send_pulse_normalized(1, motor_angles[0]);
+		rc_servo_send_pulse_normalized(2, motor_angles[1]);
+		rc_servo_send_pulse_normalized(3, motor_angles[2]);
+		rc_servo_send_pulse_normalized(4, motor_angles[3]);
 		rc_usleep(1e6 / 50);
 	}
 
